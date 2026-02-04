23fd3b05e06f9d3326c98a123ddcf2420c1302c3
Jiaoyang <90205498+kelly0924xu@users.noreply.github.com>
2026-02-04 14:09:39 -0600

    Add new functions for PTC processing in R

--- NMDesc/gene_list/get_nmdesc_region.R
+++ NMDesc/gene_list/get_nmdesc_region.R
@@ -1,1 +1,6 @@
-
+get_PTC_plus_ind <- function(input_seqs,type='plus1')
+}
+get_NMDesc_PTC = function(input_seqs,bp,variant_transcript,type='plus1')
+}
+get_NMDesc_PTC_region = function(PTC_ind,variant_transcript,PTC_status)
+}
